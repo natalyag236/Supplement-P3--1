ca9e8fb9c053941752dc3222f44bfc8d74cd2484
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-05 23:26:06 -0500

    doc: add documentation for  readFromfile function

--- readFromFile.js
+++ readFromFile.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
+/**
+ * Reads content from a file at the specified file path.
+ * 
+ * @param {string} filePath - The path to the file.
+ * @returns {string} - The content read from the file.
+ */
 
 function readFromFile(filePath) {
     try {
